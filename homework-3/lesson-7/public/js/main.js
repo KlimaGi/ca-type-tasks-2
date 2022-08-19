ca62a4d0e9890fa0846a4cdcc9b8ad294652aa57
Giedre <giedre.klima@gmail.com>
2022-08-19 17:14:19 +0300

    feat(76): stack data structure implemented

--- homework-3/lesson-7/public/js/main.js
+++ homework-3/lesson-7/public/js/main.js
@@ -2,31 +2,87 @@
 console.group('1. Dėklo (Stack) duomenų struktūros kūrimas');
 {
     class Stack {
+        static elementNotEmpty = (el) => el !== undefined;
         index;
         constructor() {
             this.index = -1;
+        }
+        get length() {
+            return this.index + 1;
         }
         push(data) {
             this.index += 1;
             this[this.index] = data;
         }
+        pop() {
+            const lastElement = this[this.index];
+            if (Stack.elementNotEmpty(lastElement)) {
+                delete this[this.index];
+                this.index -= 1;
+                return lastElement;
+            }
+            return null;
+        }
     }
     const numberStack = new Stack();
     const stringStack = new Stack();
+    const numberArrStack = new Stack();
     console.group('1.1. sukurkite konstruktorių, kuris nustatytų privačią savybę "index" į -1');
     {
+        console.log('numberStack', numberStack);
+        console.log('stringStack', stringStack);
+        console.log('numberArrStack', numberArrStack);
     }
     console.groupEnd();
-    console.groupCollapsed('1.2. Sukurkite metodą "push", kuris pridėtų elementą į struktūros galą, t.y.: vienetu didesniu indeksu nei dabartinis index. Po pridėjimo index savybę padidinkite vienetu');
+    console.group('1.2. Sukurkite metodą "push", kuris pridėtų elementą į struktūros galą, t.y.: vienetu didesniu indeksu nei dabartinis index. Po pridėjimo index savybę padidinkite vienetu');
     {
+        numberStack.push(1);
+        numberStack.push(4);
+        numberStack.push(12);
+        stringStack.push('Some');
+        stringStack.push('text');
+        stringStack.push('goes');
+        stringStack.push('here');
+        numberArrStack.push([1]);
+        numberArrStack.push([12, 2]);
+        numberArrStack.push([7, 9]);
+        console.log('numberStack', numberStack);
+        console.log('stringStack', stringStack);
+        console.log('numberArrStack', numberArrStack);
     }
     console.groupEnd();
-    console.groupCollapsed('1.3. Sukurkite metodą "pop", kuris išimtų elementą iš struktūros galo. Po išėmimo index savybę sumažinkite vienetu');
+    console.group('1.3. Sukurkite metodą "pop", kuris išimtų elementą iš struktūros galo. Po išėmimo index savybę sumažinkite vienetu');
     {
+        console.log('numberStack', numberStack.pop());
+        console.log('numberStack', numberStack.pop());
+        console.log('numberStack', numberStack.pop());
+        console.log('numberStack', numberStack.pop());
+        console.log('numberStack', numberStack.pop());
+        console.log('numberStack', numberStack);
+        console.log('stringStack', stringStack.pop());
+        console.log('stringStack', stringStack.pop());
+        console.log('stringStack', stringStack.pop());
+        console.log('stringStack', stringStack.pop());
+        console.log('stringStack', stringStack.pop());
+        console.log('stringStack', stringStack);
+        const removeValue = numberArrStack.pop();
+        console.log({ removeValue });
+        console.log('numberArrStack', numberArrStack.pop());
+        console.log('numberArrStack', numberArrStack.pop());
+        console.log('numberArrStack', numberArrStack.pop());
+        console.log('numberArrStack', numberArrStack.pop());
+        console.log('numberArrStack', numberArrStack.pop());
+        console.log('numberArrStack', numberArrStack);
     }
     console.groupEnd();
-    console.groupCollapsed('1.4. Sukurkite get\'erį "length", kuris grąžintų elementų kiekį struktūroje');
+    console.group('1.4. Sukurkite get\'erį "length", kuris grąžintų elementų kiekį struktūroje');
     {
+        console.log('numberStack.length', numberStack.length);
+        numberStack.push(6);
+        numberStack.push(7);
+        console.log('numberStack.length', numberStack.length);
+        numberStack.pop();
+        console.log('numberStack.length', numberStack.length);
     }
     console.groupEnd();
 }
--- homework-3/lesson-7/src/main.ts
+++ homework-3/lesson-7/src/main.ts
@@ -45,19 +45,38 @@ console.group('1. Dėklo (Stack) duomenų struktūros kūrimas');
   /*
     Perskaitykite: https://runestone.academy/ns/books/published/pythonds/BasicDS/WhatisaStack.html
   */
-
+  
   // ↓↓↓ klasė ↓↓↓
   class Stack<T> {
+    private static elementNotEmpty = <Type>(el: Type | undefined): el is Type => el !== undefined;
+
     private index: number;
-    [x: number]: T | undefined;
+
+   [x: number]: T | undefined;
 
     constructor(){
       this.index = -1;
     }
 
+    public get length(){
+      return this.index + 1;
+    }
+
     public push(data: T){
       this.index += 1;
       this[this.index] = data;
+    }
+
+    public pop(): T | null {
+      const lastElement = this[this.index];
+
+      if(Stack.elementNotEmpty(lastElement)){
+        delete this[this.index];
+        this.index -= 1;
+
+        return lastElement as T;
+      }
+      return null;
     }
   }
   // ↑↑↑ klasė ↑↑↑
@@ -65,30 +84,78 @@ console.group('1. Dėklo (Stack) duomenų struktūros kūrimas');
   // ↓↓↓ bendri kintamieji ↓↓↓
   const numberStack = new Stack<number>();
   const stringStack = new Stack<string>();
+  const numberArrStack = new Stack<number[]>();
   // ↑↑↑ bendri kintamieji ↑↑↑
 
   // 5 min.
   console.group('1.1. sukurkite konstruktorių, kuris nustatytų privačią savybę "index" į -1');
   {
+    console.log('numberStack', numberStack);
+    console.log('stringStack', stringStack);
+    console.log('numberArrStack', numberArrStack);
 
   }
   console.groupEnd();
 
   // 20 min
-  console.groupCollapsed('1.2. Sukurkite metodą "push", kuris pridėtų elementą į struktūros galą, t.y.: vienetu didesniu indeksu nei dabartinis index. Po pridėjimo index savybę padidinkite vienetu');
+  console.group('1.2. Sukurkite metodą "push", kuris pridėtų elementą į struktūros galą, t.y.: vienetu didesniu indeksu nei dabartinis index. Po pridėjimo index savybę padidinkite vienetu');
   {
+    numberStack.push(1);
+    numberStack.push(4);
+    numberStack.push(12);
+
+    stringStack.push('Some');
+    stringStack.push('text');
+    stringStack.push('goes');
+    stringStack.push('here');
+
+    numberArrStack.push([1]);
+    numberArrStack.push([12, 2]);
+    numberArrStack.push([7, 9]);
+    
+    console.log('numberStack', numberStack);
+    console.log('stringStack', stringStack);
+    console.log('numberArrStack', numberArrStack);
   }
   console.groupEnd();
 
   // 20 min
-  console.groupCollapsed('1.3. Sukurkite metodą "pop", kuris išimtų elementą iš struktūros galo. Po išėmimo index savybę sumažinkite vienetu');
+  console.group('1.3. Sukurkite metodą "pop", kuris išimtų elementą iš struktūros galo. Po išėmimo index savybę sumažinkite vienetu');
   {
+    console.log('numberStack', numberStack.pop());
+    console.log('numberStack', numberStack.pop());
+    console.log('numberStack', numberStack.pop());
+    console.log('numberStack', numberStack.pop());
+    console.log('numberStack', numberStack.pop());
+    console.log('numberStack', numberStack);
+
+    console.log('stringStack', stringStack.pop());
+    console.log('stringStack', stringStack.pop());
+    console.log('stringStack', stringStack.pop());
+    console.log('stringStack', stringStack.pop());
+    console.log('stringStack', stringStack.pop());
+    console.log('stringStack', stringStack);
+
+    const removeValue = numberArrStack.pop();
+    console.log({removeValue});
+     console.log('numberArrStack', numberArrStack.pop());
+     console.log('numberArrStack', numberArrStack.pop());
+     console.log('numberArrStack', numberArrStack.pop());
+     console.log('numberArrStack', numberArrStack.pop());
+     console.log('numberArrStack', numberArrStack.pop());
+     console.log('numberArrStack', numberArrStack);
   }
   console.groupEnd();
 
   // 10 min
-  console.groupCollapsed('1.4. Sukurkite get\'erį "length", kuris grąžintų elementų kiekį struktūroje');
+  console.group('1.4. Sukurkite get\'erį "length", kuris grąžintų elementų kiekį struktūroje');
   {
+    console.log('numberStack.length', numberStack.length);
+    numberStack.push(6);
+    numberStack.push(7);
+    console.log('numberStack.length', numberStack.length);
+    numberStack.pop();
+    console.log('numberStack.length', numberStack.length);
   }
   console.groupEnd();
 }
