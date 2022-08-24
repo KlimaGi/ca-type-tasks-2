2e32dde5e48ec05782ac7c971e63be29e8892657
Giedre <giedre.klima@gmail.com>
2022-08-25 00:12:20 +0300

    cao 7 linked list part task done

--- cao-lesson-7/src/main.ts
+++ cao-lesson-7/src/main.ts
@@ -8,7 +8,7 @@
   2. Sukurkite sąrašo klasę List.
     * Klasė turi privačias turėti savybes
       * head -> nuoroda į sąrašo pirmajį mazgą
-      * taip -> nuoroda į sąrašo paskutinijė mazgą
+      * taip -> nuoroda į sąrašo paskutini mazgą
     * Sukurkite konstruktorių, jog jis galėtų priimti pirmu parametru perduotą ListNode elementą ir priskirti
     jį savybėms List.head ir List.tail
     Konstruktoriaus ListNode tipo parametras neprivalomas. Neperdavus parametro, List klasės objekto savybės tail ir head turi būti null
@@ -49,31 +49,100 @@
 */
 
 // ↓↓↓ Tipai ↓↓↓
-
+type ForEachCallback<T> = (value: T) => void;
 // ↑↑↑ Tipai ↑↑↑
 
 // ↓↓↓ Klasės ↓↓↓
+class ListNode<T> {
+  constructor(
+    public data: T,
+    public next: ListNode<T> | null = null
+  ) { }
+};
 
+// 1.
+class List<Type> {
+  // 2.
+  private head: ListNode<Type> | null;
+  private tail: ListNode<Type> | null;
+
+  // 2.
+  constructor(data?: Type) {
+    if (data !== undefined) {
+      this.head = new ListNode(data);
+      this.tail = this.head;
+    } else {
+      this.head = null;
+      this.tail = null;
+    }
+  }
+
+  // 3.
+  public unshift = (data: Type): void => {
+    const newNode = new ListNode(data);
+    if (this.head === null) {
+      this.head = newNode;
+      this.tail = newNode;
+    } else {
+      newNode.next = this.head;
+      this.head = newNode;
+    }
+  };
+
+  // 4.
+  public push = (data: Type): void => {
+    const newNode = new ListNode(data);
+    if (this.tail === null) {
+      this.head = newNode;
+      this.tail = newNode;
+    } else {
+      this.tail.next = newNode;
+      this.tail = newNode;
+    }
+  };
+
+  // 5.
+  public forEach = (callback: ForEachCallback<Type>): void => {
+    if (this.head === null) return;
+
+    let currentNode: ListNode<Type> = this.head;
+
+    while (true) {
+      if (currentNode.next === null) break;
+      callback(currentNode.data);
+      currentNode = currentNode.next;
+    }
+  };
+}
 // ↑↑↑ Klasės ↑↑↑
 
 // ↓↓↓ Kintamuosius skirtus pavyzdžiams saugokite čia ↓↓↓
+// 2. |  3. |  5.
+const stringList: List<string> = new List();
+const numberList: List<number> = new List(5);
 
 // ↑↑↑ Kintamuosius skirtus pavyzdžiam saugokite čia ↑↑↑
 
 console.group('1. Sukurkitę sąrašo mazgo struktūrą ListNode, bet kokiam duomenų tipui');
 {
-
+  const stringNode1: ListNode<string> = { data: 'step', next: null };
+  const stringNode2: ListNode<string> = { data: 'forward', next: stringNode1 };
+  console.log('stringNode1', stringNode1);
+  console.log('stringNode2', stringNode2);
 }
 console.groupEnd();
 
 console.group('2. Sukurkite sąrašo klasę List');
 {
+  console.log('empty string list', stringList);
 
+  console.log('number list', numberList);
 }
 console.groupEnd();
 
 console.group('3. Sukurkite metodą pridėti elementui į sąrašo priekį.');
 {
+  console.log('string list', stringList);
 
 }
 console.groupEnd();
