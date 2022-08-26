9b62a46efc710cfeb7050234b8371b9853ac51a3
Giedre <giedre.klima@gmail.com>
2022-08-26 19:21:16 +0300

    add car constructor

--- homework-4/cao-lesson-12-crud/src/helpers/cars-collection.ts
+++ homework-4/cao-lesson-12-crud/src/helpers/cars-collection.ts
@@ -1,9 +1,23 @@
+import Brand from '../types/brand';
+import Car from '../types/car';
+import Model from '../types/model';
+
 type CarsCollectionProps = {
 
 };
 
-class CarsCollection {
 
+// private, nes inkapsuliacijos esme klasės objektų savybių pasiekiamumas turi būti ribojamas.pasiekiamos turėtų būti tik tos savybės ar metodai, kurie užtikrina objekto korektiškumo išlaikymą. 
+class CarsCollection {
+  constructor(
+    private brand: Brand[],
+    private car: Car[],
+    private model: Model[],
+  ) { }
+
+  private joinCar(car: Car): {
+
+  }
 }
 
 export default CarsCollection;
--- homework-4/cao-lesson-12-crud/src/types/car-joined.ts
+++ homework-4/cao-lesson-12-crud/src/types/car-joined.ts
@@ -2,7 +2,7 @@ import Brand from './brand';
 import Car from './car';
 import Model from './model';
 
-// visi elementai is Car, iskyrus modelId, & brand is brand'o, model os model'io. Tokiu busu igyvendinama task 1 
+// visi elementai is Car, iskyrus modelId, & brand is brand'o, model is model'io. Tokiu budu igyvendinama task 1 
 type CarJoined = Omit<Car, 'modelId'> & {
   brand: Brand['title'],
   model: Model['title'],
